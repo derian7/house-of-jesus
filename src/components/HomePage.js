bf8578e8e324ce050540b6fb60bbff3e35483981
Derian <derian@derian.com>
2023-01-18 00:05:30 -0600

    Header hover added

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import HomeHero from './HomeHero'
 
 const HomePage = () => {
   return (
     <div className='container'>
-      Home
+      <HomeHero />
     </div>
   )
 }
