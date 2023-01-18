c482e5b0332f744441926a5bf1aa890bd08c3423
Derian <robles@derian.com>
2023-01-18 14:01:09 -0600

    Added header menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,31 @@
-import React from 'react'
-import './styles/Header.css'
+import React from "react";
+import "./styles/Header.css";
 
 const Header = () => {
   return (
     <header>
-      <div className='container-wide header-container'>
-        <h4 className='header-logo'>THE HOUSE OF JESUS</h4>
-        <nav className='header-nav-menu'>
-          <a href='/'>Home</a>
-          <a href='/services'>Services</a>
-          <a href='/about'>About</a>
-          <a href='/contact'>Contact</a>
-          <a href='/donate'>Donate</a>
+      <div className="container-wide header-container">
+        <h4 className="header-logo">THE HOUSE OF JESUS</h4>
+        <nav className="header-nav-menu">
+          <ul className='header-nav-links-container'>
+            <a href="/">Home</a>
+            <a href="/services">Services</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+            <a href="/donate">Donate</a>
+          </ul>
+          <div className='header-menu-icon'>
+            <input type="checkbox" id="burger-toggle" />
+            <label for="burger-toggle" class="burger-menu">
+              <div class="line"></div>
+              <div class="line"></div>
+              <div class="line"></div>
+            </label>
+          </div>
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
